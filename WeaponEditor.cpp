b8e392d733442a6ef0e05fb61eea70f84e7dd871
Brandon T. Smith <smithbt@lafayette.edu>
2020-01-16 10:51:53 -0600

    Converted price spinBox to lineEdit (with QIntValidator) to better handle large values.

--- WeaponEditor.cpp
+++ WeaponEditor.cpp
@@ -1,11 +1,15 @@
 #include "WeaponEditor.h"
 
 WeaponEditor::WeaponEditor(QWidget *parent)
-	: QWidget(parent)
+	: QWidget(parent),
+	wpn(new Weapon())
 {
 	ui.setupUi(this);
 	setMouseTracking(true);
 	setAutoFillBackground(true);
+	QIntValidator* validInt = new QIntValidator(this);
+	validInt->setBottom(0);
+	ui.price_lineEdit->setValidator(validInt);
 }
 
 WeaponEditor::~WeaponEditor()
@@ -17,14 +21,14 @@ void WeaponEditor::setWeapon(Weapon* w)
 	wpn = w;
 	ui.level_spinBox->setValue(wpn->getLevel());
 	ui.name_lineEdit->setText(wpn->getName());
-	ui.price_spinBox->setValue(wpn->getLevel());
+	ui.price_lineEdit->setText(QString::number(wpn->getLevel()));
 }
 
 Weapon* WeaponEditor::getWeapon()
 {
 	wpn->setLevel(ui.level_spinBox->value());
 	wpn->setName(ui.name_lineEdit->text());
-	wpn->setPrice(ui.price_spinBox->value());
+	wpn->setPrice(ui.price_lineEdit->text().toInt());
 	return wpn;
 }
 
