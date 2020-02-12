ae316f2b96042451f179bf27a85a6283a5bcefa3
Brandon T. Smith <smithbt@lafayette.edu>
2020-02-12 15:43:08 -0600

    looped ability parsing in CharacterModel::read to reduce maintainence. Added more index comparisons to CharacterModel::write for safety.

--- CharacterModel.cpp
+++ CharacterModel.cpp
@@ -29,41 +29,14 @@ void CharacterModel::read(const QJsonObject& json)
 	}
 
 	// Parse Abilities
-	if (json.contains("Strength") && json.value("Strength").isObject()) {
-		QJsonObject aObj = json.value("Strength").toObject();
-		Ability* a = new Ability(this);
-		a->read(aObj);
-		map.replace(Strength, QVariant::fromValue(a));
-	}
-	if (json.contains("Dexterity") && json.value("Dexterity").isObject()) {
-		QJsonObject aObj = json.value("Dexterity").toObject();
-		Ability* a = new Ability(this);
-		a->read(aObj);
-		map.replace(Dexterity, QVariant::fromValue(a));
-	}
-	if (json.contains("Constitution") && json.value("Constitution").isObject()) {
-		QJsonObject aObj = json.value("Constitution").toObject();
-		Ability* a = new Ability(this);
-		a->read(aObj);
-		map.replace(Constitution, QVariant::fromValue(a));
-	}
-	if (json.contains("Intelligence") && json.value("Intelligence").isObject()) {
-		QJsonObject aObj = json.value("Intelligence").toObject();
-		Ability* a = new Ability(this);
-		a->read(aObj);
-		map.replace(Intelligence, QVariant::fromValue(a));
-	}
-	if (json.contains("Wisdom") && json.value("Wisdom").isObject()) {
-		QJsonObject aObj = json.value("Wisdom").toObject();
-		Ability* a = new Ability(this);
-		a->read(aObj);
-		map.replace(Wisdom, QVariant::fromValue(a));
-	}
-	if (json.contains("Charisma") && json.value("Charisma").isObject()) {
-		QJsonObject aObj = json.value("Charisma").toObject();
-		Ability* a = new Ability(this);
-		a->read(aObj);
-		map.replace(Charisma, QVariant::fromValue(a));
+	for (int aValue = Strength; aValue <= Charisma; ++aValue) {
+		QString aKey = QMetaEnum::fromType<RowIndex>().valueToKey(aValue);
+		if (json.contains(aKey) && json.value(aKey).isObject()) {
+			QJsonObject aObj = json.value(aKey).toObject();
+			Ability* a = new Ability(this);
+			a->read(aObj);
+			map.replace(aValue, QVariant::fromValue(a));
+		}
 	}
 
 	endResetModel();
@@ -78,14 +51,18 @@ void CharacterModel::write(QJsonObject& json) const
 				json.insert(key, map.at(i).toString());
 		}
 		if (map.at(i).canConvert<Resource*>()) {
-			QJsonObject rObject;
-			map.at(i).value<Resource*>()->write(rObject);
-			json.insert(key, rObject);
+			if (i == Stamina) {
+				QJsonObject rObject;
+				map.at(i).value<Resource*>()->write(rObject);
+				json.insert(key, rObject);
+			}
 		}
 		if (map.at(i).canConvert<Ability*>()) {
-			QJsonObject aObject;
-			map.at(i).value<Ability*>()->write(aObject);
-			json.insert(key, aObject);
+			if (i >= Strength && i <= Charisma) {
+				QJsonObject aObject;
+				map.at(i).value<Ability*>()->write(aObject);
+				json.insert(key, aObject);
+			}
 		}
 	}
 }
